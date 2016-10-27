3ab255e271449a984ce2f02311507b819371b6d5
Manvir Grewal <mga68@sfu.ca>
2016-10-26 18:07:31 -0700

    Convert form to partial and add show actin for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,10 +22,14 @@ class UsersController < ApplicationController
   	end
   end
 
+  def show
+  	@user = User.find(params[:id])
+  end
+
    private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :id)
   end 
 
 end
